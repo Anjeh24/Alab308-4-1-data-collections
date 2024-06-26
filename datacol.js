2c271e4995754790e2d93e55a7dae2eefaa84635
anjeh24 <junez24@yahoo.com>
2024-06-25 23:35:20 -0400

    object within array created and first row stored as key in objects

--- datacol.js
+++ datacol.js
@@ -1,6 +1,6 @@
 
 let arrs = []; //empty array assigned so multiple elements could be entered into it.
-let rows = 5; //for number of rows to be 
+let rows = 5; //for number of rows to be                                           ==========part 1======================
 let cols = 4; // for creating number of columns
 let c = 0; //setting counter for number of elements in all sub arrays within arrs
 
@@ -15,7 +15,7 @@ let arry5 = [98, "Bill", "Doctor's Assistant", 26];
 //credits geeksforgeeks
 
 
-for (let i = 0; i < rows; i++){
+for (let i = 0; i < rows; i++){                                  //======Part two===========================
     arrs[i] = [];
     for (let j = 0; j < cols; j++) {
         arrs[i][j] = c++;
@@ -30,3 +30,33 @@ arrs[4] = arry5;
 
 console.log(arrs); // set up for 2d array is working
 //finally, all arrays saved in parent array 'arrs'
+
+//===============part 3===============================
+
+let newarrs = [{
+    id: 42,
+    name: "Bruce",
+    occupation: "Knight",
+    age: 41
+},
+{
+    id: 57,
+    name: "Bob",
+    occupation: "Fry Cook",
+    age: 19
+},
+{
+    id : 63,
+    name: "Blaine",
+    occupation: "Quiz Master",
+    age: 58
+},
+{
+    id: 98,
+    name: "Bill",
+    occupation: "Doctor's Assistant",
+    age: 26
+},
+]
+
+console.log(newarrs);
